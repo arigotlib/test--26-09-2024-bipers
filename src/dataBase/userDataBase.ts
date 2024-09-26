a0faab96c191ed2b7ae605da88e2ccbe03de7222
arigotlib <ag0548472624@gmail.com>
2024-09-26 09:52:44 +0300

    first

---
+++ src/dataBase/userDataBase.ts
@@ -0,0 +1,20 @@
+import { Iuser } from "../medels/userModel"
+
+export const users: Iuser[] = [
+{
+    id: '1',
+    userName: 'admin',
+    password: 'admin'
+},
+
+{
+    id: '2',
+    userName: 'user',
+    password: 'user'
+},
+
+{
+    id: '3',
+    userName: 'test',
+    password: 'test'
+}]
